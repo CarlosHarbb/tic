e8d5edd2ba0dbc3e90650d582403daf9f9001336
CarlosHarbb <ahauhauhh@gmail.com>
2025-03-18 11:41:48 +0200

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,11 +1,11 @@
 import java.util.*;
 class Computer extends User {
-    public Computer(String name, char symbol) {
+    public Computer(String name, char symbol) {  //contructor for class
         super(name, symbol);
     }
 
-    @Override
-    public String getMove(Board board) {
+
+    public String getMove(Grid board) {   //method get move overrided from parent
         Random rand = new Random();
         char[] rows = {'A', 'B', 'C'};
         List<String> availableMoves = new ArrayList<>();
@@ -17,7 +17,8 @@ class Computer extends User {
         return availableMoves.get(rand.nextInt(availableMoves.size()));
     }
 }
-//gsgsgigsnings
+
+
 
 
 
