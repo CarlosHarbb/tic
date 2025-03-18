530e7ef074e5037fda9fdd2eb4b9783140189f42
CarlosHarbb <ahauhauhh@gmail.com>
2025-03-18 11:42:04 +0200

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,13 +1,12 @@
 import java.util.*;
 class Human extends User {
-   final private Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in); // scanner to get the input from user
 
-    public Human(String name, char symbol) {
+    public Human(String name, char symbol) {         //contructor for class
         super(name, symbol);
     }
 
-    @Override
-    public String getMove(Board board) {
+    public String getMove(Grid board) {          //method get move overrided from parent
         System.out.print("Enter your move (e.g., A1, B2): ");
         return scanner.nextLine().trim().toUpperCase();
     }
